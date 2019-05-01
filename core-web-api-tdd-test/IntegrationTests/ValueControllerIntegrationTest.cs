b4d2fa7b83e3cb85182eec2ac5986f97e78c8fcf
dinesh.dev.certification <dinesh.dev.certification@gmail.com>
2019-05-01 14:51:36 +0530

    tests changes

---
+++ core-web-api-tdd-test/IntegrationTests/ValueControllerIntegrationTest.cs
@@ -0,0 +1,16 @@
+﻿using core_web_api_tdd.Controllers;
+using Xunit;
+
+namespace core_web_api_tdd_test.IntegrationTests
+{
+    public class ValueControllerIntegrationTest
+    {
+        [Fact]
+        public void IntegrationTest1()
+        {
+            var valueController = new ValuesController();
+            var results = valueController.Get();
+            Assert.NotNull(results);
+        }
+    }
+}
---
+++ core-web-api-tdd-test/UnitTests/ValueContollerUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using core_web_api_tdd.Controllers;
+using Xunit;
+
+namespace core_web_api_tdd_test.UnitTests
+{
+    public class ValueContollerUnitTest
+    {
+        [Fact]
+        public void UnitTest1()
+        {
+            var valueController = new ValuesController();
+            var results = valueController.Get();
+            Assert.NotNull(results);
+        }
+    }
+}
